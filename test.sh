accf8e5e5a953b52e3e2d983ad9429e5bd02c6fa
Egan Ford <datajerk@gmail.com>
2017-04-02 11:10:46 -0600

    minor test cleanup

--- test.sh
+++ test.sh
@@ -7,7 +7,6 @@ ADDR=800
 SUM=898ebb103fdda6fffe62394c1b915d1d
 
 rm -f ${BIN}.dsk
-
 echo
 echo "Testing OS/X c2d..."
 bin/c2d ${BIN},${ADDR} ${BIN}.dsk 2>&1 | sed 's/^/    /'
@@ -20,6 +19,7 @@ else
 	exit 1
 fi
 
+rm -f ${BIN}.dsk
 echo
 echo "Testing Windows c2d..."
 PATH=$HOME/wine/bin:$PATH
